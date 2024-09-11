1117ce33e66ae1f448991b4336dc57bd6f71653f
tyransoft <tyransoftagency@gmail.com>
2024-09-11 13:18:26 +0200

    deploymentt

--- project/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from .views import *
-
-urlpatterns = [  
-    path('singup', user_singup, name='user_singup'),
-    path('user_logout', user_logout, name='user_logout'),
-    # other urls
-]
--- project/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from .views import *
-
-urlpatterns = [
-  path('',home,name='/'),
-  path('blogs',all_blogs,name='all_blogs'),
-  path('trip_details/<int:pk>',trip_details,name='trip'),
-  path('about_me',about,name='about')
-]
